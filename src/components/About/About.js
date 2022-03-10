6b734f9ebed1c63c3cb647b03c9ddb588481f4d1
Introdructor <yashkachave7@gmail.com>
2022-03-10 17:49:07 +0530

    Blog: Update About Info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,19 +19,18 @@ const About = () => {
           <Avatar src="hfest_avatar_2.jpg" />
         </div>
         <p>
-          Hi, I'm Anurag Hazra, a self-taught passionate FrontEnd developer from
-          India, currently working at{' '}
+          Hi, I'm Introdructor, a Wallpaper,UI designer & Android Developer from
+          India.
           <a className="about__link" href="https://razorpay.com">
             Razorpay
           </a>{' '}
-          as a FrontEnd engineer. I've been building stuff on the web since when
-          I was in 6th standard, I've made countless side projects and I also
-          posses magical powers of using react to build delightful user
+          as a Designer, I've been designing stuff since past three years
+         , I've made countless side projects and I also
+          posses magical powers of using codes to build delightful user
           interfaces.
           <br />
           <br />I also love doing <b>open source</b> development, I actively
-          maintain various notable open source projects with over, <br />
-          <b>30k+ stars on GitHub</b> and <b>50m+ hits</b>. It gives me a
+          maintain various notable open source projects. It gives me a
           wonderful feeling of achievement and joy which I cannot explain in
           words.
         </p>
